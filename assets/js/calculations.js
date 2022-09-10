9eeb67eb37e9d8290a4e8dbb282064fe8227dc46
Omkar.Shinde <omkar.shinde@ril.com>
2022-09-11 00:56:15 +0530

    calculations done

--- assets/js/calculations.js
+++ assets/js/calculations.js
@@ -6,7 +6,6 @@
 
 function calculateAmount(){
     let tableBody = document.querySelector('.table-row').children
-    // let totalAmountDiv = document.querySelector('#amount')
     let totalAmountDiv
     let totalAmount = 0
     let tax = 0
@@ -14,7 +13,6 @@ function calculateAmount(){
     let rate = 0
     let totalAmountWithTax = 0
     for(let i=0; i<tableBody.length; i++){
-        // console.log(tableBody[i].cells[4].children[0].value)
         totalAmountDiv = tableBody[i].cells[tableBody[i].cells.length-1].children[0]
         tax = Number(tableBody[i].cells[tableBody[i].cells.length-4].children[0].value) //This will give the tax rate value
         quantity = Number(tableBody[i].cells[tableBody[i].cells.length-3].children[0].value) //This will get quantity value
@@ -23,9 +21,9 @@ function calculateAmount(){
         totalAmount = quantity*rate
         totalAmountWithTax = (totalAmount+((totalAmount*tax)/100)).toFixed(2)
         totalAmountDiv.value = totalAmountWithTax
-        // console.log(totalAmountWithTax)
     }
-    // totalAmountDiv.value = totalAmount
+
+    
 }
 //=========================================================================================
 
@@ -34,23 +32,114 @@ function calculateAmount(){
 //=========================================================================================
 
 function calculateSubTotal(){
-    // console.log(document.getElementById('amount').value)
     let tableBody = document.querySelector('.table-row').children
     let subTotalDiv = document.querySelector('#sub-total-amount')
     let subTotal = 0
     for(let i=0; i<tableBody.length; i++){
-        // console.log(tableBody[i].cells[4].children[0].value)
         subTotal += Number(tableBody[i].cells[tableBody[i].cells.length-1].children[0].value)
     }
     subTotalDiv.innerText = subTotal.toFixed(2)
 
-    //Calculating Round Total Amount  discount-amount
     let TotalAmountTillDiv = document.querySelector('#total-amount')
     let discountAmount = document.querySelector('#discount-amount').value
     let totalAmountTillNow = subTotal - discountAmount
     TotalAmountTillDiv.innerText = totalAmountTillNow.toFixed(2)
+
+    
+}
+
+//=========================================================================================
+
+
+
+//=========================================================================================
+let subTotalTaxAmount = 0
+let totalTaxAmount = 0
+
+function taxDiv(){
+    if(selectedTaxIndex == 1){
+        calculateTotalTaxAmount('GST')
+    }else if(selectedTaxIndex == 2){
+        calculateTotalTaxAmount('VAT')
+    }else if(selectedTaxIndex == 3){
+        calculateTotalTaxAmount('PPN')
+    }else if(selectedTaxIndex == 4){
+        calculateTotalTaxAmount('SST')
+    }else if(selectedTaxIndex == 5){
+        calculateTotalTaxAmount('HST')
+    }else if(selectedTaxIndex == 6){
+        calculateTotalTaxAmount('TAX')
+    }
 }
 
 
+function showTaxDiv(taxName){
+    let taxDiv = document.querySelector('.total-tax-container')
+    if(taxDiv.childElementCount != 0){
+        deleteTaxDiv()
+    }
+
+    let taxHeader = document.createElement('div')
+    taxHeader.setAttribute('class',`${taxName}-total-text`)
+    taxHeader.setAttribute('id',`${taxName}-tax-added`)
+
+    let taxH = document.createElement('h3')
+    taxH.setAttribute('id',`${taxName}-header`)
+    taxH.innerText = `Total ${taxName}`
+
+    taxHeader.appendChild(taxH)
+
+
+    let taxHeader2 = document.createElement('div')
+    taxHeader2.setAttribute('class',`${taxName}-total-amount`)
+    taxHeader2.setAttribute('id',`${taxName}-tax-amount-added`)
+
+    let taxH2 = document.createElement('span')
+    taxH2.setAttribute('calss',`h4`)
+    taxH2.innerHTML = '&#8377;'
+
+    let spanEle = document.createElement('span')
+    spanEle.setAttribute('id',`${taxName}-total-amount`)
+    spanEle.innerText = totalTaxAmount
+
+    taxH2.appendChild(spanEle)
+    taxHeader2.appendChild(taxH2)
+
+    taxDiv.appendChild(taxHeader)
+    taxDiv.appendChild(taxHeader2)
+
+    calculateTotalTaxAmount(taxName)    
+}
+let taxArr = []
+function deleteTaxDiv(){
+    let taxDiv = document.querySelector('.total-tax-container')
+    taxDiv.firstElementChild.remove()
+    taxDiv.lastElementChild.remove()
+    taxArr = []
+}
+
+function deleteRowTaxCalculate(){
+    taxArr.pop()
+    taxDiv()
+}
+
+
+function calculateTotalTaxAmount(taxName){
+    let div = document.querySelector(`#${taxName}-total-amount`)
+    let tableBody = document.querySelector('.table-row').children
+
+    for(let i=0; i<tableBody.length; i++){
+        let taxVal = Number(tableBody[i].cells[tableBody[i].cells.length-4].children[0].value)
+        let quantity1 = Number(tableBody[i].cells[tableBody[i].cells.length-3].children[0].value) //This will get quantity value
+        let rate1 = Number(tableBody[i].cells[tableBody[i].cells.length-2].children[0].value) //This will get rate value
+       
+        subTotalTaxAmount = Number(((quantity1*rate1)*taxVal)/100)
+        taxArr[i] = subTotalTaxAmount
+    }
+ 
+    const sum = taxArr.reduce((partialSum, a) => partialSum + a, 0);
+    div.textContent = sum
+    
+}
 
 //Calculating subtotal ends here--------------------------------------------------------------------------
