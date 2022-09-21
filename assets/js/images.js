f03428b2d3a444160e3100911b82a346b79e2537
Omkar.Shinde <omkar.shinde@ril.com>
2022-09-21 15:03:13 +0530

    logo disappearing after some time issue fixed

--- assets/js/images.js
+++ assets/js/images.js
@@ -9,7 +9,13 @@ perviewImage = document.querySelector('.invoice-logo img')
 const loadImage = () => {
     let file = fileInput.files[0]//Getting user selected file
     if(!file) return //return if user does not select any file
-    perviewImage.src = URL.createObjectURL(file) //It will create a url of passed image file object
+    // perviewImage.src = URL.createObjectURL(file) //It will create a url of passed image file object
+    let reader = new FileReader()
+    reader.readAsDataURL(file)
+    let dataurl
+    reader.addEventListener('load',() => {
+        perviewImage.src = reader.result
+    })
 }
 
 fileInput.addEventListener('change', loadImage)
